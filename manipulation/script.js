13b35086198b4647130e0bc792ea2fbeedda3a56
Ricardo Miliotte <62629414+Miliotte@users.noreply.github.com>
2021-09-21 07:03:25 -0300

    manipulation
    Manipulation css

---
+++ manipulation/script.js
@@ -0,0 +1,8 @@
+$(".aqua").addClass("mb50") //Adicionar uma nova class
+
+$(".aqua").removeClass("mb50") //Remover a class
+
+let blue = $(".blue") // declara let
+
+blue.removeClass("blue") // remove
+blue.toggleClass("blue") //adiciona
